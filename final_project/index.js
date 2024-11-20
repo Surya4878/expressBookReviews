8e9d6ddf36d1d4e7d50335f196acfdfbcd2bae4f
Surya4878 <suryaprakash4878@gmail.com>
2024-11-20 05:24:28 -0500

    Completed tasks 1-14 for the peer review assignment

--- final_project/index.js
+++ final_project/index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
@@ -10,9 +11,26 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use(
+    '/customer',
+    session({ secret: 'fingerprint_customer', resave: true, saveUninitialized: true })
+);
+
+app.use('/customer/auth/*', function auth(req, res, next) {
+    const token = req.session.token;
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access!' });
+    }
+
+    jwt.verify(token, 'fingerprint_customer', (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Invalid token!' });
+        }
+        req.username = decoded.username;
+        next();
+    });
 });
+
  
 const PORT =5000;
 
